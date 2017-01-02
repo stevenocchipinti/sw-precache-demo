424ed1a8a068aa098083795904fc8e1543484d7c
Steve Occhipinti <dev@stevenocchipinti.com>
2017-01-02 17:01:30 +1100

    Register and use the serviceworker and sw-precache

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import registerServiceWorker from './service-worker-registration.js';
+registerServiceWorker();
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
